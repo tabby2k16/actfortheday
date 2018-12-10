f4d2ed0f150d1c4bf6a1001282a4a9ea7b4cc1f3
mark.a.r.madrona <mark.a.r.madrona@accenture.com>
2018-12-10 18:24:29 +0800

    mark commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','INR',56,0);
+
+INSERT INTO exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'USD','PHP',56,0);
+
+INSERT INTO exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'USD','EUR',56,0);
